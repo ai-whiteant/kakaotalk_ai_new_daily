5b374b3b10439c7fb01e30d15c4a12ec446257d2
하얀개미 <292332246+ai-whiteant@users.noreply.github.com>
2026-08-05 16:35:45 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+ef main():
+    print("====================================")
+    print("Hello, ChatGPT and GitHub Actions!")
+    print("파이썬 자동 빌드 테스트 성공!")
+    print("====================================")
+
+if __name__ == "__main__":
+    main()
